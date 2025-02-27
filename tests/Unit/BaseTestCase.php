3d7249143bc3e544f372bda6096f5beef942594e
Mark Dujali <mddujali@gmail.com>
2025-02-28 05:06:04 +0800

    Correct columns count unit test

--- tests/Unit/BaseTestCase.php
+++ tests/Unit/BaseTestCase.php
@@ -3,11 +3,14 @@
 namespace Tests\Unit;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
 use Tests\TestCase;
 
 abstract class BaseTestCase extends TestCase
 {
+    use RefreshDatabase;
+
     protected Model $model;
 
     protected string $table;
@@ -26,6 +29,14 @@ abstract class BaseTestCase extends TestCase
                 $this->model->getTable(),
                 $this->columns
             )
+        );
+    }
+
+    public function test_it_should_have_correct_columns_count(): void
+    {
+        $this->assertCount(
+            count(Schema::getColumnListing($this->model->getTable())),
+            $this->columns
         );
     }
 }
